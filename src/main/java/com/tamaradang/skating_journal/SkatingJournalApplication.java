9be6ec35b464d7b575efbcfc710c1f5ed7b8acc7
Tamara Dang <tamara.dang@gmail.com>
2026-07-07 10:35:52 -0400

    initial commit

---
+++ src/main/java/com/tamaradang/skating_journal/SkatingJournalApplication.java
@@ -0,0 +1,13 @@
+package com.tamaradang.skating_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkatingJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkatingJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamaradang/skating_journal/SkatingJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamaradang.skating_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkatingJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
